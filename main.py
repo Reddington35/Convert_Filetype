c0bba3811f5b0fa1ddea8dd213439520053e7cba
James Reddington <reddingtonjamesr@gmail.com>
2022-02-25 22:03:44 +0000

    python

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+text = open("ntplog.txt","rt")
+out = open("out.txt","wt")
+for i in text:
+    out.write(' '.join(i.split()))
+    if(i == "="):
+        print("\n")
+text.close()
+out.close()
+print(out)
