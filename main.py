9fc4646819a2a25c5fd8fe9cecd2f7c4ba696b52
James Reddington <reddingtonjamesr@gmail.com>
2022-02-27 17:38:43 +0000

    python

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import os
 import pandas as pd
-ntp = "ntplog.txt"
-text = open(ntp,"r")
-new_file = open("new_file.txt", "w")
-r = text.readlines()
 
-for line in r:
-    new_file.write(line.replace("=",""))
-new_file.close()
-new_file = pd.read_csv("new_file.txt")
-new_file.to_csv("new_file.csv")
-print(new_file)
+def format_csv():
+    ntp = "ntplog.txt"
+    new_ntp = "new_file.txt"
+    text = open(ntp,"r")
+    new_file = open(new_ntp,"w")
+    r = text.readlines()
+
+    for line in r:
+        new_file.write(line.replace("=",""))
+    new_file.close()
+
+    new_file = pd.read_csv("new_file.txt")
+    new_file.to_csv("new_file.csv")
+    print(new_file)
+    return 0
+
+format = format_csv()
 
 
 
