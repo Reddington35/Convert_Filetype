aad4ca094d9402e5ced1107b66ee0bc2695d67dc
James Reddington <reddingtonjamesr@gmail.com>
2022-02-27 16:09:14 +0000

    python

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-
+import pandas as pd
 text = open("ntplog.txt")
 new_file = open("new_file.txt", "w")
 r = text.readlines()
@@ -7,6 +7,9 @@ r = text.readlines()
 for line in r:
     new_file.write(line)
 new_file.close()
+
+new_file = pd.read_csv("new_file.txt")
+new_file.to_csv("new_file.csv")
 
 
 
